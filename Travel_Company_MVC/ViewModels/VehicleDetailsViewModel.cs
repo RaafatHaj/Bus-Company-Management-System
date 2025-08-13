19750488131bcfb60acd9aaad479c2ee2a82f9f6
Rafaat Haj-Isa <rafatsh.m@gmail.com>
2025-08-13 15:24:40 +0300

    New Update 10

--- Travel_Company_MVC/ViewModels/VehicleDetailsViewModel.cs
+++ Travel_Company_MVC/ViewModels/VehicleDetailsViewModel.cs
@@ -8,6 +8,7 @@
 		public string? VehicleNumber { get; set; }
 		public int? Seats { get; set; }
 		public string? HomeStation { get; set; }=null!;
+		public bool IsFromDataBase { get; set; }=false;
 
 	}
 }
